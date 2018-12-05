ffcd1fd80adc201acba1f33aeae83ae90557f4d9
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2018-12-05 11:01:00 +0200

    Added function with logger info

--- Maven/src/main/java/First.java
+++ Maven/src/main/java/First.java
@@ -1,2 +1,18 @@
+
+import org.apache.log4j.Logger;
 public class First {
+
+  private  static Logger logger = Logger.getLogger(First.class);
+
+private void systemOutputViaLogger()
+{
+    logger.info("wdwd");
+}
+    public static void main(String [] args)
+    {
+        First first = new First();
+        first.systemOutputViaLogger();
+
+    }
+
 }
